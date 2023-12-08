fdc14a021da585101f8f26ff0a66d136642dd3bc
Tsabit <tsabit@Tsabits-MacBook-Air.local>
2023-12-08 18:23:41 +0700

    add delete recycling feature

--- recycling-api/src/routes/recyclingRoutes.js
+++ recycling-api/src/routes/recyclingRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addRecycling, getUserRecyclingResults, getDetailUserRecyclingResults } = require('../controllers/recyclingController');
+const { addRecycling, getUserRecyclingResults, getDetailUserRecyclingResults, deleteUserRecycling } = require('../controllers/recyclingController');
 const authMiddleware = require('../middleware/authMiddleware');
 const { multer } = require('../utils/multerConfig');
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/add', authMiddleware, multer.single('photo'), addRecycling);
 router.get('/results', authMiddleware, getUserRecyclingResults);
 router.get('/results/:id', authMiddleware, getDetailUserRecyclingResults);
+router.delete('/delete/:id', authMiddleware, deleteUserRecycling);
 
 module.exports = router;
