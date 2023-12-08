d181021f24c5218af3d739df8108efc54593fac4
Tsabit <tsabit@Tsabits-MacBook-Air.local>
2023-12-08 18:09:32 +0700

    add delete recycling

---
+++ function-user-total-recycling/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp();
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, 'ch2-psy491');
+    req.user = decoded;
+    const userDoc = await admin.firestore().collection('users').doc(req.user.userId).get();
+    const resultsSnapshot = await admin.firestore().collection('users').doc(req.user.userId).collection('results').get();
+
+    const coins = userDoc.exists ? userDoc.data().coins || 0 : 0;
+
+    res.json({
+      coins,
+      listRecycling: resultsSnapshot.size,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+exports.getTotal = functions.https.onRequest(app);
--- recycling-api/src/controllers/recyclingController.js
+++ recycling-api/src/controllers/recyclingController.js
@@ -111,8 +111,45 @@ const getDetailUserRecyclingResults = async (req, res) => {
   }
 };
 
+const deleteUserRecycling = async (req, res) => {
+  try {
+    const resultId = req.params.id;
+    const resultRef = db.collection('users').doc(req.user.userId).collection('results').doc(resultId);
+    const result = await resultRef.get();
+
+    if (!result.exists) {
+      res.status(404).json({
+        error: true,
+        message: 'Result not found',
+      });
+      return;
+  }
+
+  await resultRef.delete();
+
+  const userRef = db.collection('users').doc(req.user.userId);
+  const user = await userRef.get();
+  const currentCoins = user.data().coins || 0;
+  const newCoins = -10;
+  const totalCoins = currentCoins + newCoins;
+  await userRef.update({ coins: totalCoins });
+
+  res.status(200).json({
+    error: false,
+    message: 'Data recycling berhasil dihapus'
+  });
+  } catch (error) {
+    console.error('Error in deleteUserRecycling:', error);
+      res.status(500).json({
+        error: true,
+        message: 'Failed to delete recycling data',
+    });
+  }
+};
+
 module.exports = {
   addRecycling,
   getUserRecyclingResults,
   getDetailUserRecyclingResults,
+  deleteUserRecycling,
 };
